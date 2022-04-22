b0ae19f3f48ddac3d0fae75f104e9952861847ab
Tobby Tan <tb@edgeai.net>
2022-04-22 22:45:03 +0800

    iè¿添加各种软件

--- js/soft_list.js
+++ js/soft_list.js
@@ -3,4 +3,8 @@ var linux_soft_list = [
     { name: "QQ", url: "https://im.qq.com/linuxqq/download.html", update_time: "2020.04.09", version: "2.0.0_b2", desc: "过时的即时聊天工具" },
     { name: "钉钉", url: "https://alidocs.dingtalk.com/i/p/nb9XJlJ7QbxN8GyA/docs/ROGpvEna5YQWmaPgQ156W4ykmK3zoB27", update_time: "", version: "1.3.0.20214", desc: "阿里巴巴旗下办公软件" },
     { name: "飞书", url: "https://www.feishu.cn/download", update_time: "", version: "V5.11.3", desc: "字节跳动旗下办公软件" },
+    { name: "网易云音乐", url: "https://d1.music.126.net/dmusic/netease-cloud-music_1.2.1_amd64_ubuntu_20190428.deb", update_time: "2019.04.28", version: "1.2.1", desc: "音乐软件" },
+    { name: "百度网盘", url: "https://pan.baidu.com/download", update_time: "", version: "V4.3.0", desc: "百度云盘软件" },
+    { name: "百度输入法", url: "https://srf.baidu.com/site/guanwang_linux/index.html", update_time: "", version: "V1.0.0", desc: "百度输入法软件" },
+    { name: "搜狗输入法", url: "https://pinyin.sogou.com/linux/?r=pinyin", update_time: "", version: "V4.0.0", desc: "搜狗输入法软件" },
 ]
