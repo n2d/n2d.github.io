ea3814cb396c17711a34c3d54d600e196858a8e0
Tobby <tobby1986@gmail.com>
2022-05-15 10:46:24 +0800

    Update soft_list.js

--- js/soft_list.js
+++ js/soft_list.js
@@ -1,5 +1,5 @@
 var linux_soft_list = [
-    { name: "微信", url: "https://archive.ubuntukylin.com/software/pool/partner/weixin_2.1.2_amd64.deb", update_time: "2022.01.28", version: "2.1.2", desc: "全国人民都知道" },
+    { name: "微信", url: "https://archive.ubuntukylin.com/software/pool/partner/weixin_2.1.3_amd64.deb", update_time: "2022.05.7", version: "2.1.3", desc: "全国人民都知道" },
     { name: "QQ", url: "https://im.qq.com/linuxqq/download.html", update_time: "2020.04.09", version: "2.0.0_b2", desc: "过时的即时聊天工具" },
     { name: "钉钉", url: "https://alidocs.dingtalk.com/i/p/nb9XJlJ7QbxN8GyA/docs/ROGpvEna5YQWmaPgQ156W4ykmK3zoB27", update_time: "", version: "1.3.0.20214", desc: "阿里巴巴旗下办公软件" },
     { name: "飞书", url: "https://www.feishu.cn/download", update_time: "", version: "V5.11.3", desc: "字节跳动旗下办公软件" },
